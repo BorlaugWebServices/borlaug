559c193082e1a4c45de5c13a93abd97570791030
Timothy Whitehead <twhiteheadzm@gmail.com>
2020-10-05 11:26:07 +0200

    version and schema update

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -80,11 +80,11 @@ pub mod opaque {
 pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_name: create_runtime_str!("borlaug-chain"),
     impl_name: create_runtime_str!("borlaug-chain"),
-    authoring_version: 1,
-    spec_version: 1,
-    impl_version: 1,
+    authoring_version: 2,
+    spec_version: 2,
+    impl_version: 2,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 1,
+    transaction_version: 2,
 };
 
 /// The version information used to identify this runtime when compiled natively.
