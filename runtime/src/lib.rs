5fe61bb94c3e8189ae40e5b9e3a6c0781282653f
Timothy Whitehead <twhiteheadzm@gmail.com>
2021-07-22 21:26:08 +0200

    add audits benchmark

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1339,6 +1339,7 @@ impl_runtime_apis! {
 
             add_benchmark!(params, batches, pallet_groups, Groups);
             add_benchmark!(params, batches, pallet_identity, Identity);
+            add_benchmark!(params, batches, pallet_audits, Audits);
 
             if batches.is_empty() { return Err("Benchmark not found for this pallet.".into()) }
             Ok(batches)
